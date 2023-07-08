47aaac8ed325ec90769722e42ed667cbb9d67355
yetan12 <pflaumekunze@gmail.com>
2023-07-08 15:11:31 +0200

    m

--- Scripts/scare_zone.gd
+++ Scripts/scare_zone.gd
@@ -4,7 +4,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	play()
+	$AnimatedSprite2D.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
