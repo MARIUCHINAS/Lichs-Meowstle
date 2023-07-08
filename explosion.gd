5eaa76971e49922c886f16c7ea982270da33f61f
MARIUCHINAS <mariususaitis@gmail.com>
2023-07-08 22:06:00 +0300

    Meeh

--- explosion.gd
+++ explosion.gd
@@ -10,3 +10,4 @@ func _process(delta):
 
 func _on_area_2d_body_entered(body):
 	get_tree().get_root().get_node("Main").health -= 5
+	get_tree().get_root().get_node("Main").get_node("Enemy").get_node("MeehAudioStreamPlayer").play()
