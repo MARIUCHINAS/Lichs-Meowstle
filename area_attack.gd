b9ba352e76f212a2fb3e8f5b93490011db469717
yetan12 <pflaumekunze@gmail.com>
2023-07-09 08:47:05 +0200

    k

--- area_attack.gd
+++ area_attack.gd
@@ -6,7 +6,7 @@ var matrix=[
 			[1,0,0,0,0,0,1],
 			[1,0,0,0,0,0,1],
 			[1,0,0,0,0,0,1],
-			[0,1,0,0,0,1,0],
+			[0,1,0,0,0,1,0],#0
 			[0,0,1,1,1,0,0]],
 
 			[[0,0,0,0,0,0,0],
@@ -14,13 +14,13 @@ var matrix=[
 			[0,0,1,1,1,1,0],
 			[0,0,1,1,1,0,0],
 			[0,1,1,1,1,0,0],
-			[0,0,0,0,1,0,0],
+			[0,0,0,0,1,0,0],#1
 			[0,0,0,0,0,0,0]],
 			
 			[[0,0,0,1,0,0,0],
 			[0,0,0,1,0,0,0],
 			[0,0,0,1,0,0,0],
-			[1,1,1,1,1,1,1],
+			[1,1,1,1,1,1,1],#2
 			[0,0,0,1,0,0,0],
 			[0,0,0,1,0,0,0],
 			[0,0,0,1,0,0,0]],
@@ -30,16 +30,24 @@ var matrix=[
 			[0,0,0,0,0,0,0],
 			[0,0,0,0,0,0,0],
 			[0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0],#3
 			[1,1,1,1,1,1,1]],
 			
 			[[1,0,0,0,0,0,1],
 			[1,0,0,0,0,0,1],
 			[1,0,0,0,0,0,1],
 			[1,0,0,0,0,0,1],
+			[1,0,0,0,0,0,1],#4
 			[1,0,0,0,0,0,1],
-			[1,0,0,0,0,0,1],
-			[1,0,0,0,0,0,1]]
+			[1,0,0,0,0,0,1]],
+			
+			[[1,1,1,1,1,1,1],
+			[1,1,1,1,1,1,1],
+			[1,1,1,1,1,1,1],
+			[1,1,1,1,1,1,1],#5
+			[1,1,1,1,1,1,1],
+			[1,1,1,1,1,1,1],
+			[1,1,1,1,1,1,1]]
 			]
 var matrix_size=7
 var explosion=load("res://explosion.tscn")
@@ -55,7 +63,7 @@ func _ready():
 func _on_timer_timeout():
 	for x in range(matrix_size):
 		for y in range(matrix_size):
-			if matrix[3][y][x]==1:
+			if matrix[5][y][x]==1:
 				var current=explosion.instantiate()
 				current.position=(Vector2(x,y)-Vector2(1,1)*matrix_size*.5+Vector2(0.5,.5))*spacing
 				add_child(current)
@@ -65,6 +73,6 @@ func _on_timer_timeout():
 	var healthmod=(100-health)*.01+1
 	var distancemod=50000/(distance*distance)
 	if distancemod>=5:distancemod=5
-	var mod =distancemod*healthmod-3
+	var mod =distancemod*healthmod-2
 	print(mod)
-	get_tree().get_root().get_node("Main").energy+=distancemod*healthmod
+	get_tree().get_root().get_node("Main").energy+=mod
