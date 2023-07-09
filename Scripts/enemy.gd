afd4e8e69b665ced0c47647aa998b5152c57f172
yetan12 <pflaumekunze@gmail.com>
2023-07-09 09:29:33 +0200

    Update enemy.gd

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -16,7 +16,7 @@ var speed
 var targetpos=Vector2(0,0)
 var collision
 
-@export var base_speed = 3
+@export var base_speed = 100
 
 @onready var stats = $Stat
 
@@ -48,11 +48,12 @@ func _physics_process(delta):
 		
 	updatevel()
 	if collision:
-		velocity.slide(collision.get_normal())
+		velocity=velocity.slide(collision.get_normal())
 		velocity=velocity.normalized()*speed
 		print("sliding")
-	print(velocity)
-	collision = move_and_collide(velocity)
+		print(collision.get_normal())
+	#print(velocity)
+	collision = move_and_collide(velocity*delta)
 
 	#move_and_slide()
 	#print(collision)
